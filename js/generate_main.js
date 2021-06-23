2e35e9be4c0403f9f7806c053efc8dc04c6517fb
linhdd-dev <doduylinh.it@gmail.com>
2021-06-23 18:18:54 +0700

    fix(image): resize image after load

--- js/generate_main.js
+++ js/generate_main.js
@@ -62,10 +62,9 @@ const EnumPageType = {
             scroll_element.addEventListener('scroll', _eventHandler);
         }
 
-        _addWindowResizeEvent = () => {
+        _addWindowResizeEvent = (callback) => {
             const _eventHandler = (e) => {
-                const _product_cart_top_el = document.querySelectorAll('div[class="omp-product-card__top"]');
-                _product_cart_top_el.forEach(e => e.style.height = `${e.offsetWidth}`);
+                callback(e);
             };
 
             window.addEventListener('resize', _eventHandler);
@@ -158,7 +157,7 @@ const EnumPageType = {
                             <div  class="omp-menu-item ng-star-inserted">
                                 <div  class="omp-menu-item--content active">
                                     <p>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
                                             <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
                                             <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
                                         </svg>
@@ -169,7 +168,7 @@ const EnumPageType = {
                             <div  class="omp-menu-item ng-star-inserted">
                                 <div class="omp-menu-item--content" onclick="omipage.open_cart_popup()">
                                     <p>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
                                             <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
                                         </svg>
                                     </p>
@@ -179,7 +178,7 @@ const EnumPageType = {
                             <div  class="omp-menu-item ng-star-inserted">
                                 <div  class="omp-menu-item--content">
                                     <p>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
                                             <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
                                         </svg>
                                     </p>
@@ -388,6 +387,11 @@ const EnumPageType = {
             this._getElementConfigs().then();
         }
 
+        _resizeProductImage = () => {
+            const _product_cart_top_el = document.querySelectorAll('div[class="omp-product-card__top"]');
+            _product_cart_top_el.forEach(e => e.style.height = `${e.offsetWidth}`);
+        }
+
         _renderAllProductBlock = async (page) => {
             const response = await this.data_service.getLandingProductAsync(page);
             const response_obj = JSON.parse(response);
@@ -403,6 +407,8 @@ const EnumPageType = {
                     this.page_configs.currency || 'đ'
                 );
             }
+
+            this._resizeProductImage();
         }
 
         _renderMoreProductOfLanding = async (page) => {
@@ -501,7 +507,7 @@ const EnumPageType = {
                 _loadMoreProduct
             );
 
-            this.global_event._addWindowResizeEvent();
+            this.global_event._addWindowResizeEvent(this._resizeProductImage);
         }
     }
 
