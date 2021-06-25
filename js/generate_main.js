0aee735d641c88934503d78b6dd5783ef570fa4c
linhdd-dev <doduylinh.it@gmail.com>
2021-06-25 18:32:48 +0700

    fix(): change cart icon

--- js/generate_main.js
+++ js/generate_main.js
@@ -162,9 +162,13 @@ const EnumPageType = {
 
         _headerElementBuilder = (landing_name, image_url) => {
             return `<div class="omp-header-block--content">
+                        <div class="omp-header--space"></div>
                         <img  alt="Landing Logo" src="${image_url}">
-                        <div  class="omp-header--landing-name">${landing_name}</div>
-                        <div  class="omp-header--space"></div>
+                        <div class="omp-header--cart">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                            </svg>
+                        </div>
                     </div>`
         }
 
@@ -185,8 +189,8 @@ const EnumPageType = {
                             <div  class="omp-menu-item ng-star-inserted">
                                 <div class="omp-menu-item--content" onclick="omipage.open_cart_popup()">
                                     <p>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
-                                            <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
+                                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
                                         </svg>
                                     </p>
                                     <span>Giỏ hàng</span>
@@ -512,6 +516,12 @@ const EnumPageType = {
                         this.page_configs.locale || DefaultVNLocale.VN_ICU_LOCALE,
                         this.page_configs.currency || DefaultVNLocale.VN_CURRENCY_CODE
                     );
+                    console.log(this.content_builder._outstandingProductContentBuilder(
+                        config.element_title,
+                        arr_products,
+                        this.page_configs.locale || DefaultVNLocale.VN_ICU_LOCALE,
+                        this.page_configs.currency || DefaultVNLocale.VN_CURRENCY_CODE
+                    ));
                     break;
 
                 case EnumLandingBlockElementName.DESIGN_FLASH_SALE:
